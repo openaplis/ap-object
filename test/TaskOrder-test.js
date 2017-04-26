b7282bda996cecfc52934ecc2de01ab9ad41317a
SidHarder <softwarenavigator@gmail.com>
2017-04-26 09:53:33 -0600

    working on task order interface

--- test/TaskOrder-test.js
+++ test/TaskOrder-test.js
@@ -6,7 +6,7 @@ const tsk = require('../src/core/Task-Generic')
 
 var testAO = null;
 var masterAccessionNo = '17-8171'
-var prefix = 'TSK'
+var taskPrefix = 'TSKO'
 
 describe('Task Order Object Tests', function() {
 
@@ -20,10 +20,11 @@ describe('Task Order Object Tests', function() {
 
   describe('New Tests', function() {
     it('Is data being passed in correctly', function(done) {
-      var to = taskOrder.new(testAO.AccessionOrder.PanelSetOrders[0], tsk, 9999, 'OP')
-      assert.equal(to.TaskOrder.MasterAccessionNo, '17-8171', 'The MasterAccessionNo is not corret.')
-      assert.equal(to.TaskOrder.TaskName, tsk.Task.TaskName, 'The Task name is not correct.')
-      done()
+      var to = taskOrder.newItem(testAO.AccessionOrder.PanelSetOrders[0], tsk, 9999, 'OP', function (err, to) {
+        assert.equal(to.TaskOrder.MasterAccessionNo, '17-8171', 'The MasterAccessionNo is not corret.')
+        assert.equal(to.TaskOrder.TaskName, tsk.Task.TaskName, 'The Task name is not correct.')
+        done()
+      })
     })
   })
 
@@ -34,7 +35,7 @@ describe('Task Order Object Tests', function() {
         assert.notEqual(err, null)
         done()
       })
-    })    
+    })
 
     it('Should return error if TestOrders is not an array', function(done) {
       taskOrder.nextId(masterAccessionNo, 123, function (err, nextId) {
@@ -45,18 +46,34 @@ describe('Task Order Object Tests', function() {
 
     it('Should return 1st ID if TestOders is an empty array', function(done) {
       taskOrder.nextId(masterAccessionNo, [], function (err, nextId) {
-        assert.notEqual(nextId, masterAccessionNo + '.' + prefix + '01')
+        assert.equal(nextId, masterAccessionNo + '.' + taskPrefix + '1')
         done()
       })
     })
 
     it('Should return 2nd ID if TestOders has 1 item', function(done) {
-      var to1 = taskOrder.blank()
-      to1.TaskOrder.TaskOrderId = masterAccessionNo + '.' + prefix + '01'
+      var to1 = taskOrder.blankItem()
+      to1.TaskOrder.TaskOrderId = masterAccessionNo + '.' + taskPrefix + '1'
       var taskOrders = [to1]
 
       taskOrder.nextId(masterAccessionNo, taskOrders, function (err, nextId) {
-        assert.notEqual(nextId, masterAccessionNo + '.' + prefix + '01')
+        assert.equal(nextId, masterAccessionNo + '.' + taskPrefix + '2')
+        done()
+      })
+    })
+
+    it('Does it work with 11 TaskOrders?', function(done) {
+      var taskOrders = []
+      var i = 1
+      while (i <= 11) {
+        var to = taskOrder.blankItem()
+        to.TaskOrder.TaskOrderId = masterAccessionNo + '.' + taskPrefix + i
+        taskOrders.push(to)
+        i ++
+      }
+
+      taskOrder.nextId(masterAccessionNo, taskOrders, function (err, nextId) {
+        assert.equal(nextId, masterAccessionNo + '.' + taskPrefix + '12')
         done()
       })
     })
